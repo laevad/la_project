3689fc5f721defb7563aaf4431e98a314e40566a
laevad <aldave.tabian@ustp.edu.ph>
2022-11-08 11:09:11 +0800

    added seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserTypeSeeder::class,
             AdminSeeder::class,
+            InspectionTypeSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/InspectionTypeSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class InspectionTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('inspection_types')->insert([
+            ['name'=>'Inspection'],
+            ['name'=>'Investigation'],
+            ['name'=>'Admin'],
+            ['name'=>'Othe Docs'],
+        ]);
+    }
+}
